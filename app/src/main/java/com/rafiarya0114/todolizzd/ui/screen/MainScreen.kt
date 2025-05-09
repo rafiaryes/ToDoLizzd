51af5e0958fbfe33e8ab6cd1e7fde10c182f9f23
Rafi Arya <rafiaryaramadhan2710@gmail.com>
2025-05-09 23:20:07 +0700

    Membuat Tampilan list

--- app/src/main/java/com/rafiarya0114/todolizzd/ui/screen/MainScreen.kt
+++ app/src/main/java/com/rafiarya0114/todolizzd/ui/screen/MainScreen.kt
@@ -1,8 +1,15 @@
 package com.rafiarya0114.todolizzd.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -11,8 +18,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.rafiarya0114.todolizzd.R
+import com.rafiarya0114.todolizzd.model.Task
 import com.rafiarya0114.todolizzd.ui.theme.ToDoLizzdTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -37,10 +49,47 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android",
-        modifier = modifier
-    )
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
+    LazyColumn(
+        modifier = modifier.fillMaxSize()
+    ) {
+        items(data){
+            ListItem(task = it)
+            HorizontalDivider()
+        }
+    }
+}
+
+@Composable
+fun ListItem(task: Task) {
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = task.title,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = task.desc,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+
+        )
+        Text(
+            text = stringResource(
+                when (task.priority) {
+                    1 -> R.string.tinggi
+                    2 -> R.string.sedang
+                    else -> R.string.rendah
+                }
+            )
+        )
+        Text(text = stringResource(if (task.isDone) R.string.sudah else R.string.belum))
+    }
 }
 
 @Preview(showBackground = true)
