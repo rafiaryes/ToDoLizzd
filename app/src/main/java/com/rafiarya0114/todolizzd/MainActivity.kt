00b3013ca649aaed7a01ad03ae103ef73c475980
Rafi Arya <rafiaryaramadhan2710@gmail.com>
2025-05-09 23:41:25 +0700

    menambahkan navigation component

--- app/src/main/java/com/rafiarya0114/todolizzd/MainActivity.kt
+++ app/src/main/java/com/rafiarya0114/todolizzd/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.rafiarya0114.todolizzd.navigation.SetupNavGraph
-import com.rafiarya0114.todolizzd.ui.screen.MainScreen
 import com.rafiarya0114.todolizzd.ui.theme.ToDoLizzdTheme
 
 class MainActivity : ComponentActivity() {
